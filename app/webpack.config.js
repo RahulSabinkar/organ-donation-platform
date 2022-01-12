6affd06d4c0b5a2c0aae440de7d591c684fed448
Rahul Sabinkar <rahulsabinkar4u@gmail.com>
2022-01-12 19:56:35 +0530

    added path for css, images to webpack

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -10,6 +10,10 @@ module.exports = {
   },
   plugins: [
     new CopyWebpackPlugin([{ from: "./src/index.html", to: "index.html" }]),
+    new CopyWebpackPlugin([{ from: "./src/css/bootstrap.css", to: "css/bootstrap.css" }]),
+    new CopyWebpackPlugin([{ from: "./src/css/style1.css", to: "css/style1.css" }]),
+    new CopyWebpackPlugin([{ from: "./src/css/fontawesome-all.css", to: "css/fontawesome-all.css" }]),
+    new CopyWebpackPlugin([{ from: "./src/images/organ-donation.jpg", to: "images/organ-donation.jpg" }]),
   ],
   devServer: { contentBase: path.join(__dirname, "dist"), compress: true },
 };
